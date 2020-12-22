539923ad86d5de908491d5a704b4599e9511c38d
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2020-12-22 13:59:00 +0530

    Update max_min.cpp

--- max_min.cpp
+++ max_min.cpp
@@ -1,10 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 void find_max_min(int a[],int n)
-{
+{	// First Mehod
+	/*
 	sort(a,a+n);
 	cout<<"Max:"<<a[n-1]<<endl;
 	cout<<"Min:"<<a[0];
+	*/
+	// Second Method
+	int max=a[0],min=a[0],i;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>max)
+			max=a[i];
+		if(a[i]<min)
+			min=a[i];
+	}
+	cout<<"Max:"<<max<endl;
+	cout<<"Min:"<<min;
 }
 int main()
 {
