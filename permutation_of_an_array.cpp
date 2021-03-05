0f84c66e8ef4df078d54e0e9fc375428038ff23a
Uttam Anand <54016758+uttam-anand@users.noreply.github.com>
2021-03-05 21:57:56 +0530

    Update permutation_of_an_array.cpp

--- permutation_of_an_array.cpp
+++ permutation_of_an_array.cpp
@@ -16,3 +16,35 @@ public:
     }
     
 };
+
+
+                            // Second Method to do the same problem using the method of backtracking :
+
+
+class Solution {
+public:
+    void permute_helper(vector < vector< int>>&ans,  vector < int> nums, int start)
+    {
+        if(start == nums . size())
+        {
+            ans. push_back(nums);
+            return ;
+        }
+        
+        for( int i=start; i< nums.size() ;i++)
+        {
+            swap(nums [i], nums[start]);
+            
+            permute_helper(ans,  nums, start +1);
+            
+            swap(nums [i] , nums[start]);
+                
+        }
+        
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector < vector< int > >ans;
+        permute_helper(ans, nums ,0);
+    return ans;
+    }
+};
